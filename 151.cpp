df88cf942533b6874ab10b950cb941e89bf35743
tephen <523193924@qq.com>
2021-04-14 22:31:03 +0800

    wrong 151

---
+++ 151.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    void removeSpace(string& s){
+        int slow=0,fast=0;
+        if(s.size()==0)return ;
+        while(fast<s.size()&&s[fast]==' ')++fast;//去除开头空格
+        while(fast<s.size()){//去除中间空格
+            if(s[fast]==' '&&s[fast-1]==' ')++fast;
+            else s[slow++]=s[fast++];
+        }
+        while(s[slow]==' ')--slow;//去除尾部空格
+        s.resize(slow+1);
+        return;
+    }
+    string reverseWords(string s) {
+        removeSpace(s);//去除空格
+        reverse(s.begin(),s.end());//全串反转
+        int wordBegin=0,wordEnd=0;//单词反转
+        for(;wordEnd<s.size();++wordEnd){
+            if(s[wordEnd]!=' ')continue;
+            reverse(s.begin()+wordBegin,s.begin()+wordEnd);
+            wordBegin = wordEnd+1;
+        }
+        reverse(s.begin()+wordBegin,s.end());
+        return s;
+    }
+};
