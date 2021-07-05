bf5d9affb210079e66b41063f4678c0fa8084858
tephen <523193924@qq.com>
2021-07-05 12:23:18 +0800

    sudoku

---
+++ 37.py
@@ -0,0 +1,34 @@
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+
+        def backTrack(board) ->bool:
+            def notValid(row,col,num):
+                for i in range(0,9):
+                    if board[row][i]==num:
+                        return True
+                    if board[i][col]==num:
+                        return True
+                startRow,startCol = (row//3)*3,(col//3)*3
+                for i in range(startRow,startRow+3):
+                    for j in range(startCol,startCol+3):
+                        if board[i][j]==num:
+                            return True
+                return False
+
+            for row in range(0,9):
+                for col in range(0,9):
+                    if board[row][col]!=".":
+                        continue
+                    for num in [str(n) for n in range(1,10)]:
+                        if notValid(row,col,num):
+                            continue
+                        board[row][col] = num
+                        if backTrack(board):
+                            return True
+                        board[row][col] = "."
+                    return False
+            return True
+        backTrack(board)
